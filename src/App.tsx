ef775bf25e439efeab9efd662d527f55d4915b69
zhbhun <zhbhun@gmail.com>
2022-05-27 15:28:13 +0800

    feat: 集成 ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import { ConfigProvider, DatePicker, message } from 'antd';
+import 'antd/dist/antd.less';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment, { Moment } from 'moment';
+import 'moment/dist/locale/zh-cn';
+
+moment.locale('zh-cn');
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState<Moment | null>(null);
+  const handleChange = (value: Moment | null) => {
+    message.info(
+      `您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`
+    );
+    setDate(value);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <ConfigProvider locale={zhCN}>
+      <div>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </div>
+      <div>
+        <DatePicker onChange={handleChange} />
+        <div style={{ marginTop: 16 }}>
+          当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+        </div>
+      </div>
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
