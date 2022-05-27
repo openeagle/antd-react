d9007792e5780d11b1c4aa2723a8f75805dd750e
zhbhun <zhbhun@gmail.com>
2022-05-27 15:48:48 +0800

    feat: 集成 tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import 'antd/dist/antd.less';
 import zhCN from 'antd/lib/locale/zh_CN';
 import moment, { Moment } from 'moment';
 import 'moment/dist/locale/zh-cn';
+import logo from './logo.svg';
 
 moment.locale('zh-cn');
 
@@ -19,15 +20,26 @@ function App() {
 
   return (
     <ConfigProvider locale={zhCN}>
-      <div>
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is: {count}
-        </button>
-      </div>
-      <div>
-        <DatePicker onChange={handleChange} />
-        <div style={{ marginTop: 16 }}>
-          当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+      <div className="mx-auto container">
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+          <div className="shrink-0">
+            <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-slate-500">You have a new message!</p>
+          </div>
+        </div>
+        <div>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </div>
+        <div>
+          <DatePicker onChange={handleChange} />
+          <div style={{ marginTop: 16 }}>
+            当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+          </div>
         </div>
       </div>
     </ConfigProvider>
